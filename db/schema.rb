d1aed8422696a5dfcdb4f0121019578733ae957b
Collins <mutaic84@gmail.com>
2022-06-29 12:16:14 +0300

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -37,4 +37,13 @@ ActiveRecord::Schema.define(version: 2022_06_21_202135) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
+    t.index ["email"], name: "index_users_on_email", unique: true
+  end
+
 end
